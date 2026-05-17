f7d3a237671ea4111baa4d6c6b9f991142c1df7c
Administrator <gitlab_admin_dc706f@example.com>
2026-05-16 20:56:33 -0400

    modify urls files into apps and project

--- django_crud_api/urls.py
+++ django_crud_api/urls.py
@@ -39,7 +39,7 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
-    path('',include('tasks.urls'))
+    path('tasks/',include('tasks.urls'))
 ]
 
 urlpatterns += [
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path,include
 from rest_framework import routers
 from .views import TaskView
+
+
 router = routers.DefaultRouter()
 
-router.register(r'task',TaskView,'tasks')
+router.register(r'',TaskView,basename='tasks')
 
 urlpatterns = [
-    path("api/v1/", include (router.urls)),
+    path("", include (router.urls)),
 ]
